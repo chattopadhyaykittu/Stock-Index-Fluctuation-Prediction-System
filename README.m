2c98dc4edfbe59f2c773c5aa783878914f21ed3b
chattopadhyaykittu <https://github.com/chattopadhyaykittu/test_repo.git>
2022-12-07 20:48:49 -0500

    Initial Commit

---
+++ README.m
@@ -0,0 +1,5 @@
+Summary:
+
+Conducted a research on how the headlines could impact the stock market. The headlines are crawled and NLP sentiment analysis is conducted. After this, the final results are shared to the subscribers via email.
+
+Tech Stack : Python (Selenium, BeautifulSoup, NLTK, TextBlob, Snorkel), GCP storage bucket, Mailchimp API, HTML, CSS, Bootstrap, and Node.js
